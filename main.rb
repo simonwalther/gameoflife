b0bc1af2131588d857be0193742838dfda731181
Simon Walther <simon.walther@liquid-concept.ch>
2014-02-21 14:42:52 +0100

    create class cell and class board

--- main.rb
+++ main.rb
@@ -1,2 +1,37 @@
 
 #!/usr/bin/env ruby
+
+class Board
+  attr_accessor :width, :height, :numbercell
+
+  def initialize
+    @width = 10
+    @height = 10
+    @numbercell = @width * @height
+  end
+end
+
+class Cell
+  attr_accessor :posx, :posy, :alive
+
+  def initialize(posx, posy)
+    #positions x and y
+    @posx = posx
+    @posy = posy
+
+    #alive true or nil
+    @alive = true
+  end
+end
+
+board = Board.new
+
+celllist = Array.new
+numbercell = board.numbercell
+
+#create cells
+numbercell.times do
+  celllist << Cell.new(1, 1)
+end
+
+puts "#{celllist}"
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,1 +1,22 @@
 require 'spec_helper'
+
+describe Board do
+  context 'after created' do
+    subject { Board.new }
+
+    its(:width) { should == 10 }
+    its(:height) { should == 10 }
+    its(:numbercell) { should == 100 }
+  end
+end
+
+describe Cell do
+  context 'after created' do
+    subject { Cell.new(1, 1) }
+
+    its(:posx) { should == 1 }
+    its(:posy) { should == 1 }
+
+    its(:alive) { should == true }
+  end
+end
