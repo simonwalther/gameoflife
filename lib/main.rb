d6d6d9135487230947005696e56447e30881c185
Simon Walther <simon.walther@liquid-concept.ch>
2014-02-26 09:18:10 +0100

    make the basics rules

--- lib/main.rb
+++ lib/main.rb
@@ -9,6 +9,7 @@ class Main
 
     @celllist = Array.new
     @numbercell = board.numbercell
+    @width = board.width
 
     ##### initialize cells #####
     a = 1
@@ -19,9 +20,52 @@ class Main
 
       a += 1
 
-      if a == (board.width + 1)
+      if a == (@width + 1)
         a = 1
         b += 1
+      end
+    end
+
+    z = 1
+    x = 1
+
+    numbercell.times do
+      selected = celllist.select { |a| a.posx == z && a.posy == x }
+      slct = selected[0]
+
+      slct.neighbour << [slct.posx,   slct.posy+1]
+      slct.neighbour << [slct.posx+1, slct.posy]
+      slct.neighbour << [slct.posx+1, slct.posy+1]
+      slct.neighbour << [slct.posx,   slct.posy-1]
+      slct.neighbour << [slct.posx-1, slct.posy]
+      slct.neighbour << [slct.posx-1, slct.posy-1]
+      slct.neighbour << [slct.posx+1, slct.posy-1]
+      slct.neighbour << [slct.posx-1, slct.posy+1]
+
+      if z == 4 && x == 4
+      ############################### case: restera vivante ##############################
+        slct.alive = true
+
+        toaliveslct = celllist.select { |a| a.posx == slct.neighbour[0].first && a.posy == slct.neighbour[0].last }
+        ngbslct = toaliveslct[0]
+
+        ngbslct.alive = true
+
+        toaliveslct = celllist.select { |a| a.posx == slct.neighbour[1].first && a.posy == slct.neighbour[1].last }
+        ngbslct = toaliveslct[0]
+
+        ngbslct.alive = true
+
+      elsif z == 7 && x == 4
+      ############################### case: meurt ##############################
+        slct.alive = true
+      end
+
+      z += 1
+
+      if z == (@width + 1)
+        z = 1
+        x += 1
       end
     end
     ############################
@@ -34,34 +78,65 @@ class Main
   end
 
   def isalive
-    selected = celllist.select { |a| a.posx == 1 && a.posy == 1 }
+    z = 1
+    x = 1
 
-    puts "selected: #{selected}"
-    slct = selected[0]
+    numbercell.times do
+      selected = celllist.select { |a| a.posx == z && a.posy == x }
+      slct = selected[0]
 
-    puts "slctposx: #{slct.posx}"
-    puts "slctposy: #{slct.posy}"
-    @neighbour << [slct.posx,   slct.posy+1]
-    @neighbour << [slct.posx+1, slct.posy]
-    @neighbour << [slct.posx+1, slct.posy+1]
-    @neighbour << [slct.posx,   slct.posy-1]
-    @neighbour << [slct.posx-1, slct.posy]
-    @neighbour << [slct.posx-1, slct.posy-1]
-    @neighbour << [slct.posx+1, slct.posy-1]
-    @neighbour << [slct.posx-1, slct.posy+1]
+      b = 0
+      c = 0
 
-    puts "neighbour: #{@neighbour}"
+      if slct.alive == true && slct != nil
+        8.times do |a|
+          neighbourslct = celllist.select { |a| a.posx == slct.neighbour[b].first && a.posy == slct.neighbour[b].last }
+          ngbslct = neighbourslct[0]
 
-    8.times do |a|
-      neightbourslct = celllist.select { |a| a.posx == neighbour[@posx] && a.posy == neighbour[@posy] }
+          if ngbslct != nil
 
-      if neightbourslct[@alive] == true
-        b += 1
+            if ngbslct.alive == true
+              c += 1
+            end
+          end
+
+          b += 1
+        end
+
+        if c == 2 || c == 3
+          puts "la cellule #{slct.posx};#{slct.posy} restera vivante"
+        else
+          puts "la cellule #{slct.posx};#{slct.posy} meurt"
+        end
+      elsif slct.alive == false && slct != nil
+        8.times do |a|
+          neighbourslct = celllist.select { |a| a.posx == slct.neighbour[b].first && a.posy == slct.neighbour[b].last }
+          ngbslct = neighbourslct[0]
+
+          if ngbslct != nil
+
+            if ngbslct.alive == true
+              c += 1
+            end
+          end
+
+          b += 1
+        end
+
+        if c == 3
+          puts "la cellule #{slct.posx};#{slct.posy} nait"
+        else
+          puts "la cellule #{slct.posx};#{slct.posy} restera morte"
+        end
       end
-    end
 
-    if b == 2
-      puts "restera vivante"
+      c = 0 #reset
+      z += 1
+
+      if z == (@width + 1)
+        z = 1
+        x += 1
+      end
     end
   end
 end
