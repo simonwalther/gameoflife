032e43f318a5c44e99d088895cb9b0997ea24123
Simon Walther <simon.walther@liquid-concept.ch>
2014-04-14 11:43:21 +0200

    make a rules parser

--- lib/gameoflife/cli.rb
+++ lib/gameoflife/cli.rb
@@ -4,11 +4,12 @@ require "gameoflife/game"
 
 module Gameoflife
   class Cli < Thor
-     # play [--rule=conway] --tick=1000 --result=result.txt file.txt
+     # play [--rule=conway] --tick=1000
      # generate --width --height --file=file.txt
      # rules
 
     DEFAULT_BOARD_PATH = File.expand_path(__FILE__ + '/../../../config/alive.txt')
+    DEFAULT_RULES_PATH = File.expand_path(__FILE__ + '/../../../config/rules.txt')
 
     desc "generate <BOARD FILE>", "generate the board file with the good width and height"
     option :width, type: :numeric, default: 10, aliases: :w
@@ -32,18 +33,97 @@ module Gameoflife
 
     desc "play <BOARD FILE>", "generate the board file with another file and run it"
     option :ticks, type: :numeric, default: 500, aliases: :t
-    def play(board_path = DEFAULT_BOARD_PATH)
+    option :dry, type: :boolean, default: false, aliases: :d
+    option :sleep_time, type: :numeric, default: 0.15, aliases: :s
+    option :rules, type: :string, default: "conway", aliases: :r
+
+    def play(board_path = DEFAULT_BOARD_PATH, rules_path = DEFAULT_RULES_PATH)
       board = Board.new(board_path)
       game = Game.new(board)
+      sleep_time = options[:sleep_time]
+      nb_ticks = options[:ticks]
+      rules = options[:rules]
+      rules_file = File.open(rules_path, "r+")
+      read_name = true
+      read_cases_of_life = false
+      read_cases_of_birth = false
+      end_of_cases = false
+      name = Array.new
+      cases_of_life = Array.new
+      cases_of_birth = Array.new
+
+      until name == rules && end_of_cases == true || rules_file.eof? == true
+        rules_file.size.times do
+          select_char = rules_file.getc.chr
+
+          if read_name == true
+            name << select_char
+          elsif read_cases_of_life == true
+            cases_of_life << select_char
+          elsif read_cases_of_birth == true
+            cases_of_birth << select_char
+          elsif end_of_cases == true && name != rules
+            name = Array.new
+            cases_of_life = Array.new
+            cases_of_birth = Array.new
+          end
+
+          if select_char == "{"
+            if (name.is_a? String) == false
+              name = (name.join.chomp('{')).gsub!(/\s+/, "")
+            end
+            read_name = false
+            read_cases_of_life = true
+          elsif select_char == ","
+            read_cases_of_life = false
+            read_cases_of_birth = true
+          elsif select_char == "}"
+            read_cases_of_birth = false
+            end_of_cases = true
+          elsif end_of_cases == true && read_cases_of_birth == false && read_cases_of_life == false && read_name == false && name != rules
+            end_of_cases = false
+            read_name = true
+          end
+        end
+      end
+
+      cases_of_life = cases_of_life.join.chomp(',').to_i
+      cases_of_birth = cases_of_birth.join.chomp("}\n").to_i
+      puts "[NAME = #{name}] [LIFE = #{cases_of_life}] [BIRTH = #{cases_of_birth}]"
+
+      if options[:dry]
+        sleep_time = 0
+      end
 
       board.displayboard
 
-      options[:ticks].times do |t|
-        puts "t: #{t}"
-        game.isalive
-        board.displayboard
-        sleep(0.15)
+      nb_ticks.times do |current_tick|
+        sleep(sleep_time)
         system "clear" or system "cls"
+
+        if options[:dry] == false
+          puts "current_tick: #{current_tick}"
+        else
+          progress = ((current_tick+1)*100)/nb_ticks
+          puts "progress: #{progress} %"
+        end
+
+        game.isalive
+
+        if options[:dry] == false || (current_tick+1) == nb_ticks
+          board.displayboard
+        end
+      end
+    end
+
+    desc "rules <RULES_PATH> <NAME> <CASES_OF_LIFE> <CASES_OF_BIRTH>", "define a rule in the rules.txt file"
+    option :name, aliases: :n, :required => true
+    option :life, aliases: :l, :required => true
+    option :birth, aliases: :b, :required => true
+
+    def rules(rules_path = DEFAULT_RULES_PATH)
+      File.open(rules_path, "a") do |w|
+        w.puts "#{options[:name]} {#{options[:life]},#{options[:birth]}}"
       end
     end
   end
